4ffefed7cc06f13bc9cd8662fbee201e11a61bd9
monikanimmana <monikanimmana3366@gmail.com>
2026-07-11 17:39:54 +0530

    adding vector db logic

---
+++ app/vector_store.py
@@ -0,0 +1,35 @@
+import chromadb #type:ignore
+from rag import read_pdf , create_chunk
+from embeddings import create_embed
+
+client = chromadb.Client()
+
+collection = client.get_or_create_collection(
+    name="documents"
+)
+
+################## Storing Embedding in vector database ############################ 
+
+def store_embed(chunks , embedding):
+
+    ids = [str(i) for i in range(len(chunks))]
+
+    collection.add(
+        ids=ids,
+        documents=chunks,
+        embeddings=embeddings
+    )
+
+################### return all embeddings ########################
+
+def get_all():
+
+    return collection.get()
+
+
+################# Return the number of stored chunks.#######################
+
+def count():
+
+    return collection.count()
+

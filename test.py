29d48aefcfb3e500ae4a75b8524401a294e4e28b
monikanimmana <monikanimmana3366@gmail.com>
2026-07-11 16:02:07 +0530

    added chunk test logic in test.py

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
-from app.rag import read_pdf
+from app.rag import read_pdf , create_chunk
 
 text = read_pdf("documents/python.pdf")
 
 print(len(text))
+
+chunks=create_chunk("documents/python.pdf")
+
+print("Number of chunks:",len(chunks))
+
+print("------------------------------")
+
+print(chunks[0])
+
+if len(chunks)>1:
+    print(chunks[1])
